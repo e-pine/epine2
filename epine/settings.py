22b51589a93ca45d777df39f86de00fd28a38e30
e-pine <117518594+JayReonal@users.noreply.github.com>
2024-01-21 00:40:53 +0800

    database add

--- epine/settings.py
+++ epine/settings.py
@@ -108,21 +108,21 @@ CHANNEL_LAYERS = {
     },
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ep_final',
-        'USER': 'root',
-        'PASSWORD': 'root1234',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ep_final',
+#         'USER': 'root',
+#         'PASSWORD': 'root1234',
+#     }
+# }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
