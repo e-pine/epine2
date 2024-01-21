30ed5c67717e232987d8c7d00e3fc4f0c11cf034
e-pine <117518594+JayReonal@users.noreply.github.com>
2024-01-21 23:20:05 +0800

    static17

--- epine/settings.py
+++ epine/settings.py
@@ -187,7 +187,6 @@ MEDIA_URL = 'images/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_SSL_REDIRECT = True
 
 
