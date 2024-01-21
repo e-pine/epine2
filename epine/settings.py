c198a9bf705b8516a8927d3dc955fa16a0a67e9a
e-pine <117518594+JayReonal@users.noreply.github.com>
2024-01-21 11:43:22 +0800

    Add SECURE_REFERRER_POLICY setting

--- epine/settings.py
+++ epine/settings.py
@@ -172,6 +172,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+SECURE_REFERRER_POLICY = ['same-origin']
 
 STATIC_URL = 'static/'
 
